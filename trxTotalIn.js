58c4a0beba069fc12062df3583bdccf237273dbf
ShangxingSun <36012471+ShangxingSun@users.noreply.github.com>
2019-01-28 17:02:23 -0800

    debug

--- trxTotalIn.js
+++ trxTotalIn.js
@@ -1,12 +1,22 @@
 var redis = require('redis');
-var client = redis.createClient();
+var client = redis.createClient(6379,"seoul-redis.bqjdlm.ng.0001.apn2.cache.amazonaws.com");
 
 const express = require('express')
-const app = express()
+const app = express();
 const port = 3000
 
 client.on('connect', function() {
     console.log('Redis client connected');
+});
+
+
+client.get('toatalinandout:gameid:10', function (error, result) {
+    if (error) {
+        console.log(error);
+        throw error;
+    }
+    console.log('GET result ->' + result);
+    res.send(result);
 });
 
 app.get('/trxTotalIn', (req, res) => {
