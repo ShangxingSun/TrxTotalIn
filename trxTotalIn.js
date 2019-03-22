03bc1d3b8a4f701d298755595f50402e66d448eb
Ubuntu <ubuntu@ip-10-2-49-75.us-east-2.compute.internal>
2019-03-22 21:20:30 +0000

    update

--- trxTotalIn.js
+++ trxTotalIn.js
@@ -1,4 +1,5 @@
 var redis = require('redis');
+var schedule = require('node-schedule')
 var client = redis.createClient(6379,"seoul-redis.bqjdlm.ng.0001.apn2.cache.amazonaws.com");
 
 const express = require('express')
@@ -11,12 +12,20 @@ client.on('connect', function() {
     console.log('Redis client connected');
 });
 
+var j = schedule.scheduleJob('0 5 0 * * 0', function() {
+	    //autoClear();
+	//    saveBeforeClear(()=>{
+	//          autoClear();
+	//              })
+		updateDividend();
+	 });
+/*
 setInterval(()=>{
   let currDate = new Date();
   let currWeekDay = currDate.getUTCDay();
   let currHour = currDate.getUTCHours()
   let currMinute = currDate.getUTCMinutes()
-  if (currWeekDay == 0 && currHour == 0 && currMinute < 5) {
+  if (currWeekDay == 0 && currHour == 0 && currMinute < 6&& currMinute > 5) {
     if(hasUpdate==false){
         updateDividend();
         hasUpdate = true;
@@ -26,10 +35,10 @@ setInterval(()=>{
   }
 },1000);
 
-
+*/
 
 app.get('/', (req, res) => {
-	console.log("get address");
+//	console.log("get address");
     client.get('toatalinandout:gameid:10', function (error, result) {
         if (error) {
             console.log(error);
@@ -43,12 +52,15 @@ app.get('/', (req, res) => {
 
 function updateDividend(){
     client.get('toatalinandout:gameid:10', function (error, result) {
+	console.log(result);
+	result = JSON.parse(result);
         var current_divident = (result.TotalIn-result.TotalOut)*0.7*0.2;
         var totalStats = {
             PlayerMined : result.PlayerMined,
             OtherMined : result.OtherMined,
             TotalIn : result.TotalIn,
-            TotalOut : result.TotalOut+current_divident
+            TotalOut : result.TotalOut+current_divident/0.7,
+	    TotalDivident : result.TotalDivident+current_divident
         };
         console.log("updateDividend",JSON.stringify(totalStats));
         client.SET('toatalinandout:gameid:10', JSON.stringify(totalStats));
--- updateTotalTronInAndOut.js
+++ updateTotalTronInAndOut.js
@@ -1,13 +1,29 @@
 var redis = require('redis');
 var client = redis.createClient(6379,"seoul-redis.bqjdlm.ng.0001.apn2.cache.amazonaws.com");
 client.get('toatalinandout:gameid:10', function (error, result) {
-    var current_divident = 227288.978000;
+	console.log(result);
+	result = JSON.parse(result);
+    var current_divident = (result.TotalIn-result.TotalOut)*0.7*0.2;
+
+   	/* 
     var totalStats = {
         PlayerMined : result.PlayerMined,
         OtherMined : result.OtherMined,
-        TotalIn : result.TotalIn,
-        TotalOut : result.TotalOut+current_divident
+        TotalIn :result.TotalIn,
+        TotalOut : result.TotalOut+current_divident/0.7*0.3,
+	TotalDivident: current_divident+result.TotalDivident
     };
+ 	*/
+	
+    var totalStats = {
+        PlayerMined : 358561.25,
+        OtherMined : 538187.1,
+        TotalIn :50172320,
+        TotalOut : 49079808.4496,
+	TotalDivident: 418478.499
+    };
+    
+    console.log(totalStats);
     console.log("updateDividend",JSON.stringify(totalStats));
     client.SET('toatalinandout:gameid:10', JSON.stringify(totalStats));
 });
